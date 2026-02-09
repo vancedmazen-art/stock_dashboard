8515d852acd7d8fc1a07d934fdf1cb92a4db81c2
vancedmazen-art <vancedmazen@gmail.com>
2026-02-09 17:54:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -273,9 +273,7 @@ with tab3:
 		
 		# 🔥 GROUP BY TICKER and get MAX PnL per stock
 		max_per_stock = df_closed_other.groupby('Ticker')['Trade_PnL_%'].max().reset_index()
-		max_per_stock['Days_Held'] = df_closed_other.loc[
-			df_closed_other.groupby('Ticker')['Trade_PnL_%'].idxmax()
-		]['Days_Held'].values
+		max_per_stock['Days_Held'] = df_closed_other.loc[df_closed_other.groupby('Ticker')['Trade_PnL_%'].idxmax()]['Days_Held'].values
 		
 		# Get top 10 stocks by max PnL
 		top_gainers = fix_pyarrow_df(max_per_stock.nlargest(10, "Trade_PnL_%")[["Ticker", "Trade_PnL_%", "Days_Held"]])
