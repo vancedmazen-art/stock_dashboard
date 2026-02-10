c103e6ffa8e3a7403b41b7c92bcbd7b4785f75cd
vancedmazen-art <vancedmazen@gmail.com>
2026-02-10 15:06:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -224,7 +224,7 @@ with tab2:
         if len(current_stock_df) > 0:
             st.markdown("#### 🟢 **CURRENT TRADES**")
             st.dataframe(fix_pyarrow_df(current_stock_df[['Entry_Date','BUY_REASON', 'Entry_Price', 'Trade_PnL_%', 
-                                                         'Days_Held', 'Exit_Support','Exit_Resistance', 'Best_Strategy']].rename(columns={
+                                                         'Days_Held', 'Exit_Support','Exit_Resistance']].rename(columns={
                                                         'Exit_Support': 'Current_Support',
                                                         'Exit_Resistance': 'Current_Resistance'
         })), use_container_width=True, height=200)
