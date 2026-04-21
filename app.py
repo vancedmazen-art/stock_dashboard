c515691e6c54cb1c01511cb676b965576217ea20
vancedmazen-art <vancedmazen@gmail.com>
2026-04-21 17:31:16 +0200

    Set width for fresh buys dataframe display

--- app.py
+++ app.py
@@ -197,6 +197,7 @@ with tab1:
             fresh_buys_display,
             use_container_width=True,
             height=500,
+            width=100,
             on_select="rerun",
             selection_mode="single-row",
             key="fresh_buys_table"
