b34a9673c57a38d527a427ecac2238281583372b
vancedmazen-art <vancedmazen@gmail.com>
2026-04-21 17:10:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ with tab1:
     #col3.metric("📊 Avg PnL", f"{new_buys['Trade_PnL_%'].mean():.1f}%" if len(new_buys)>0 else "-")
     st.dataframe(fix_pyarrow_df(new_buys_with_strategy[['Ticker', 'Entry_Date', 'Entry_Price', 'Entry_Volume','Breaks_Trendline','Stop_Loss','Target_Price','Risk_%','Reward_%', 'RR_Ratio']]), use_container_width=True, height=300)
 
-    take_profit = df_current_other[df_current_internal['Target_Hit_Date'] == refresh_date_obj].copy()
+    take_profit = df_current_other[(df_current_internal['Target_Hit_Date'] == refresh_date_obj)&(df_current_internal['Bars_To-Target'] != 0)].copy()
     take_profit_with_strategy = take_profit.merge(df_strategy[['Ticker', 'Best_Strategy']], on='Ticker', how='left')
     
     st.markdown("#### 🎯 **Take Profit**")
