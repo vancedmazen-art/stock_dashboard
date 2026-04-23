06dfc4d9cb31b105f7196d97eab97bac2c578848
vancedmazen-art <vancedmazen@gmail.com>
2026-04-23 10:26:49 +0200

    Change Testing Anchor checkbox logic

--- app.py
+++ app.py
@@ -335,8 +335,8 @@ with tab4:
         )
     with col2:
         Testing_Anchor = st.checkbox(
-            "🏢 Show Only Testing Anchor",
-            value=True,
+            "🏢 Show Only not Testing Anchor",
+            value=False,
             key="Testing_Anchor"
         )
     
@@ -347,7 +347,7 @@ with tab4:
     if ticker_filter != 'ALL':
         filtered_history = filtered_history[filtered_history['Ticker'] == ticker_filter]
     if Testing_Anchor:  # Only if selections made
-        filtered_history = filtered_history[filtered_history['Testing_Anchor'] == True]
+        filtered_history = filtered_history[filtered_history['Testing_Anchor'] == False]
     
     # 🔥 AGGREGATE METRICS (only if Trade_PnL_% exists and has data)
     if 'Trade_PnL_%' in filtered_history.columns and len(filtered_history) > 0:
