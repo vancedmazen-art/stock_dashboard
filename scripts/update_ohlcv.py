6611651b610543511af4b25805d893e73c0fe8cf
vancedmazen-art <vancedmazen@gmail.com>
2026-04-26 15:24:40 +0300

    Update update_ohlcv.py

--- scripts/update_ohlcv.py
+++ scripts/update_ohlcv.py
@@ -3,7 +3,7 @@ import time
 import traceback
 import pandas as pd
 from datetime import datetime
-from egxpy.download import get_OHLCV_data
+from scripts.data_provider import get_OHLCV_data
 
 # =============================
 # CONFIG
