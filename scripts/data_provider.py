84fe9e66b4802b680762a88d18d4456bd11770f5
vancedmazen-art <vancedmazen@gmail.com>
2026-04-26 15:25:06 +0300

    Implement get_OHLCV_data function for mock data
    Added a function to generate mock OHLCV data.

---
+++ scripts/data_provider.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+def get_OHLCV_data(symbol, exchange="EGX", interval="Daily", n_bars=1000):
+
+    # TEMP MOCK DATA (so pipeline works)
+    dates = pd.date_range(end=pd.Timestamp.today(), periods=200)
+
+    df = pd.DataFrame({
+        "datetime": dates,
+        "open": np.random.rand(len(dates)) * 100,
+        "high": np.random.rand(len(dates)) * 100,
+        "low": np.random.rand(len(dates)) * 100,
+        "close": np.random.rand(len(dates)) * 100,
+        "volume": np.random.randint(1000, 5000, len(dates)),
+    })
+
+    return df
